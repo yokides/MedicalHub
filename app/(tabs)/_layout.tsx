2dbef822c7cf955b4371ac3fc58c072f6a9d33ae
yokides <Jiraphat6575@gmail.com>
2023-10-19 05:39:09 +0700

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,55 +1,124 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-
-import Colors from '../../constants/Colors';
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import Colors from "@/constants/Colors";
+import { FontAwesome, FontAwesome5, Ionicons, Octicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router"
+import { View, Text } from "react-native";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    return (
+      <Tabs screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          bottom: 0,
+          right: 0,
+          left: 0,
+          height: 85,
+          elevation: 0,
+          backgroundColor: '#FFF'
+        }
       }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+        <Tabs.Screen
+          name="Home"
+          options={{
+            title: "",
+            tabBarIcon: ({ focused }: { focused: boolean }) => {
+              return (
+                <View style={{
+                  alignItems: "center",
+                  paddingTop: 17,
+                }}>
+                  <Ionicons
+                    name={focused ? 'home' : 'home-outline'}
+                    size={29}
+                    color={focused ? Colors.primary : Colors.medium}
                   />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+  
+                  <Text style={{
+                    fontSize: 8,
+                    color: focused ? Colors.primary : Colors.medium
+                  }}>Home</Text>
+                </View>
+              )
+            }
+          }}
+        />
+        <Tabs.Screen
+          name="Calendar"
+          options={{
+            title: "",
+            tabBarIcon: ({ focused }: { focused: boolean }) => {
+              return (
+                <View style={{
+                  alignItems: "center",
+                  paddingTop: 18,
+                }}>
+                  <FontAwesome5
+                    name={focused ? 'calendar-alt' : 'calendar'}
+                    size={30}
+                    color={focused ? Colors.primary : Colors.medium}
+                  />
+  
+                  <Text style={{
+                    fontSize: 8,
+                    color: focused ? Colors.primary : Colors.medium
+                  }}>Calendar</Text>
+                </View>
+              )
+            }
+          }}
+        />
+        <Tabs.Screen
+          name="Notification"
+          options={{
+            title: "",
+            tabBarIcon: ({ focused }: { focused: boolean }) => {
+              return (
+                <View style={{
+                  alignItems: "center",
+                  paddingTop: 18,
+                }}>
+                  <FontAwesome
+                    name={focused ? 'bell' : 'bell-o'}
+                    size={30}
+                    color={focused ? Colors.primary : Colors.medium}
+                  />
+  
+                  <Text style={{
+                    fontSize: 8,
+                    color: focused ? Colors.primary : Colors.medium
+                  }}>Notification</Text>
+                </View>
+              )
+            }
+          }}
+        />
+        <Tabs.Screen
+          name="Profile"
+          options={{
+            title: "",
+            tabBarIcon: ({ focused }: { focused: boolean }) => {
+              return (
+                <View style={{
+                  alignItems: "center",
+                  paddingTop: 18,
+                }}>
+                  <Octicons
+                    name={focused ? 'person-fill' : 'person'}
+                    size={30}
+                    color={focused ? Colors.primary : Colors.medium}
+                  />
+  
+                  <Text style={{
+                    fontSize: 8,
+                    color: focused ? Colors.primary : Colors.medium
+                  }}>Profile</Text>
+                </View>
+              )
+            }
+          }}
+        />
+  
+      </Tabs>
+    )
+  }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -16,4 +16,14 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
   },
+
+  /********************เพิ่ม***********************/
+  primary: '#1EA4A5',
+  lightGrey: '#FCF8FF',
+  grey: '#EEE9F0',
+  medium: '#9F9AA1',
+  green: '#437919',
+  mediumDark: '#3B566E'
 };
+
+
